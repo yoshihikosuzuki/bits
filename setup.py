00e8db2358899e2e1ed1639528fb208a32373cc1
yoshihikosuzuki <ys.neoteny@gmail.com>
2018-07-05 10:52:38 +0200

    use setup.cfg

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from setuptools import setup, find_packages
-import sys
+from setuptools import setup
 
-
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
-with open('requirements.txt') as f:
-    requirements = f.read().strip().split('\n')
-
-print('\033[92m' + '[INFO] You must install pbcore for python3 from https://github.com/yoshihikosuzuki/pbcore' + '\033[0m')
-
-setup(
-    name='BITS',
-    version='0.0.1',
-    description='BioInformatics ToolS',
-    long_description=readme,
-    author='Yoshihiko Suzuki',
-    author_email='ys.neoteny@gmail.com',
-    install_requires=requirements,
-    url='https://github.com/yoshihikosuzuki/BITS',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+setup()
