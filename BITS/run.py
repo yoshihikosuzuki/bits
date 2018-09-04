ab9f0db877f89f865e23e0d283e6cad8768519c2
yoshihikosuzuki <ys.neoteny@gmail.com>
2018-09-04 23:28:55 +0200

    refactor edlib

--- BITS/run.py
+++ BITS/run.py
@@ -1,29 +1,153 @@
 from logzero import logger
+import edlib
+from .seq import revcomp
+from dataclasses import dataclass
+
+
+@dataclass(repr=False)
+class Cigar:
+    string: str
+
+    def iter(self):
+        """
+        Generator of a series of tuples (num. of bases, operator).
+        """
+
+        length = ""
+        for c in self.string:
+            if c == '=' or c == 'D' or c == 'I' or c == 'X' or c == 'N':
+                yield (int(length), c)
+                length = ""
+            else:
+                length += c
+
+    @property
+    def alignment_len(self):
+        """
+        Alignment length including gaps and masked regions.
+        """
+
+        return sum([l for l, op in self.iter()])
+
+    def reversed(self):
+        """
+        Just reverse CIGAR without swapping in/del. Used for reverse complement.
+        """
+
+        return ''.join(reversed([f"{l}{op}" for l, op in self.iter()]))
+
+    def flatten(self):
+        """
+        Convert CIGAR to a sequence of operations. Used for masking some specific intervals.
+        """
+
+        return FlattenCigar(''.join([op for l, op in self.iter() for i in range(l)]))
+
+    def mask_intvl(self, intvl, ignore_op='D'):
+        # TODO: how to do about I/D/X around intervals' boundaries
+
+        cigar_f = self.flatten()
+    
+        starts = [i[0] for i in intvl]
+        ends = [i[1] for i in intvl]
+        index = 0
+        pos = 0
+        for i, c in enumerate(cigar_f):
+            if index >= len(starts):
+                break
+            if i != 0 and c != ignore_op:
+                pos += 1
+            if pos > ends[index]:
+                index += 1
+            if index < len(starts) and starts[index] <= pos and pos <= ends[index]:   # NOTE: end-inclusive
+                cigar_f[i] = 'N'
+
+        return cigar_f.unflatten()
+
+
+@dataclass(repr=False)
+class FlattenCigar:
+    string: str
+
+    def unflatten(self):
+        """
+        Convert a series of oprations to a normal CIGAR string.
+        """
+
+        cigar = ""
+        count = 0
+        prev_c = self.string[0]
+        for c in self.string:
+            if c == prev_c:
+                count += 1
+            else:
+                cigar += f"{count}{prev_c}"
+                count = 1
+                prev_c = c
+        cigar += f"{count}{prev_c}"
+        return Cigar(cigar)
+
 
 edlib_mode = {"global": "NW", "glocal": "HW", "local": "SHW"}
 
 
-def cigar_to_len(cigar):
-    alignment_len = 0
-    d = ''
-    for c in cigar:
-        if c == '=' or c == 'D' or c == 'I' or c == 'X':
-            alignment_len += int(d)
-            d = ''
+class Align:
+    def __init__(self, edlib_ret):
+        # NOTE: coordinates are on the target sequence
+        self.start, self.end = edlib_ret["locations"][0]
+        self.end += 1   # for compatibility with python's slice
+
+        self.edit_dist = edlib_ret["editDistance"]
+        self.cigar = Cigar(edlib_ret["cigar"])
+        self.length = self.cigar.alignment_len
+        self.diff = self.edit_dist / self.length
+
+        self.strand = 0   # set as forward (0) by default
+
+    def show(self):
+        logger.info(f"{'revcomp(' if self.strand == 1 else ''}target[{self.start}:{self.end}]{')' if self.strand == 1 else ''} ({self.length} bp, {self.diff:.3} diff)")
+        logger.info(f"{self.cigar.string}")
+
+
+def _find_best_align(query,
+                     target,
+                     mode,
+                     rc,
+                     rc_pruning_diff_th,
+                     strand_prior):
+    if not rc:
+        align = Align(edlib.align(query, target, mode=mode, task="path"))
+    else:
+        if strand_prior == 0:
+            align_f = Align(edlib.align(query, target, mode=mode, task="path"))
+            if align_f.diff < rc_pruning_diff_th:
+                align = align_f
+            else:
+                target_rc = revcomp(target)
+                align_rc = Align(edlib.align(query, target_rc, mode=mode, task="path"))
+                if align_f.diff <= align_rc.diff:
+                    align = align_f
+                else:
+                    align = align_rc
+                    align.strand = 1
+                    target = target_rc
         else:
-            d += c
-    return alignment_len
+            target_rc = revcomp(target)
+            align_rc = Align(edlib.align(query, target_rc, mode=mode, task="path"))
+            if align_rc.diff < rc_pruning_diff_th:
+                align = align_rc
+                align.strand = 1
+                target = target_rc
+            else:
+                align_f = Align(edlib.align(query, target, mode=mode, task="path"))
+                if align_f.diff <= align_rc.diff:
+                    align = align_f
+                else:
+                    align = align_rc
+                    align.strand = 1
+                    target = target_rc
 
-
-def revcomp_cigar(cigar):
-    ret = ""
-    while len(cigar) > 0:
-        cut_point = 0
-        while cigar[cut_point] not in set(['=', 'X', 'I', 'D']):
-            cut_point += 1
-        ret = cigar[0:cut_point + 1] + ret
-        cigar = cigar[cut_point + 1:]
-    return ret
+    return (align, target)
 
 
 def run_edlib(query,
@@ -32,11 +156,10 @@ def run_edlib(query,
               only_diff=False,
               rc=False,
               rc_pruning_diff_th=0.3,   # at most this diff if true strand
-              strand_prior=None,   # 0 or 1
+              strand_prior=0,   # 0 or 1
               cyclic=False,
               return_seq=False,   # in glocal alignment
               return_seq_diff_th=1.0,   # remove distant sequences
-              return_cigar=False,
               swap_cigar=False):
     """
     Perform 1 vs 1 pairwise sequence alignment using edlib.
@@ -51,16 +174,10 @@ def run_edlib(query,
     NOTE: local alignment using edlib is not recommended.
     """
 
-    # TODO: implement <revcomp_query> (or <multi_target>?) and  <count_bad_align>
-
     assert mode in ["global", "glocal", "local"], f"Invalid mode name: {mode}"
     assert query != "" and target != "", "Input sequence must not be empty!"
 
-    import edlib
-    if rc:
-        from .seq import revcomp
-
-    # NOTE: glocal cyclic can return a sequence shorter than the original one
+    # NOTE: glocal cyclic possibly returns a shorter sequence than the original one
     global_cyclic = False
     if cyclic:
         target = target * 2
@@ -68,113 +185,64 @@ def run_edlib(query,
             global_cyclic = True
             mode = "glocal"
 
-    mode = edlib_mode[mode]
+    # Find best start/end and strand between the two sequences
+    # If revcomp of target is the best, change target to it
+    align, target = _find_best_align(query,
+                                     target,
+                                     edlib_mode[mode],
+                                     rc,
+                                     rc_pruning_diff_th,
+                                     strand_prior)
 
-    strand = 0
-    if not rc:
-        align = edlib.align(query, target, mode=mode, task="path")
-        diff = align["editDistance"] / cigar_to_len(align["cigar"])
-    else:
-        if strand_prior is None or strand_prior == 0:
-            align_f = edlib.align(query, target, mode=mode, task="path")
-            diff_f = align_f["editDistance"] / cigar_to_len(align_f["cigar"])
-            if diff_f < rc_pruning_diff_th:
-                align = align_f
-                diff = diff_f
-            else:
-                target_rc = revcomp(target)
-                align_rc = edlib.align(query, target_rc, mode=mode, task="path")
-                diff_rc = align_rc["editDistance"] / cigar_to_len(align_rc["cigar"])
-                if diff_f <= diff_rc:
-                    align = align_f
-                    diff = diff_f
-                else:
-                    strand = 1
-                    align = align_rc
-                    diff = diff_rc
-                    target = target_rc
-        else:
-            target_rc = revcomp(target)
-            align_rc = edlib.align(query, target_rc, mode=mode, task="path")
-            diff_rc = align_rc["editDistance"] / cigar_to_len(align_rc["cigar"])
-            if diff_rc < rc_pruning_diff_th:
-                strand = 1
-                align = align_rc
-                diff = diff_rc
-                target = target_rc
-            else:
-                align_f = edlib.align(query, target, mode=mode, task="path")
-                diff_f = align_f["editDistance"] / cigar_to_len(align_f["cigar"])
-                if diff_f <= diff_rc:
-                    align = align_f
-                    diff = diff_f
-                else:
-                    strand = 1
-                    align = align_rc
-                    diff = diff_rc
-                    target = target_rc
-
-    # NOTE: coordinates are on <target>
-    start, end = align["locations"][0]
-    end += 1   # for compatibility with python's slice
-
-    flag_cycle = False
+    flag_cycle = False   # True if the alignment was actually cyclic
     if cyclic:
+        # Adjust start/end points to the original coordinates
         half_pos = len(target) // 2
-        if end > half_pos:
-            end -= half_pos
+        if align.end > half_pos:
+            align.end -= half_pos
             flag_cycle = not flag_cycle
-            if start >= half_pos:
-                start -= half_pos
+            if align.start >= half_pos:
+                align.start -= half_pos
                 flag_cycle = not flag_cycle
 
+        # Restore the duplicated target sequence to the original one
         target = target[:half_pos]
 
+        # Adjust the difference between glo"b"al cyclic (expected) and glo"c"al mapping (firstly done)
         if global_cyclic:
             if flag_cycle:
-                end = start
-                target_re = target[start:] + target[:end]
+                align.end = align.start
+                target_re = target[align.start:] + target[:align.end]
             else:   # adjust so that the alignment is same as global mode
-                start = 0
-                end = half_pos
-                target_re = target[start:end]
-            align_re = run_edlib(query, target_re, "global", return_cigar=True)   # computation time is doubled but this is easy....
-            diff = align_re["diff"]
-            align["cigar"] = align_re["cigar"]
+                align.start = 0
+                align.end = half_pos
+                target_re = target[align.start:align.end]
+            # NOTE: taking alignment again is redundant, but very easy
+            align_re = run_edlib(query, target_re, "global")
+            align.diff = align_re.diff
+            align.cigar = align_re.cigar
 
     if only_diff:
-        return diff
+        return align.diff
 
     if return_seq:
-        if diff > return_seq_diff_th:
-            seq = None
+        if align.diff > return_seq_diff_th:
+            align.seq = None
         else:
             if flag_cycle:
-                seq = target[start:] + target[:end]
+                align.seq = target[align.start:] + target[:align.end]
             else:
-                seq = target[start:end]
+                align.seq = target[align.start:align.end]
 
-    if strand == 1:
-        start_tmp = start
-        start = len(target) - end
-        end = len(target) - start_tmp
+    if align.strand == 1:
+        tmp = align.start
+        align.start = len(target) - align.end
+        align.end = len(target) - tmp
 
-    ret = {"start": start, "end": end, "diff": diff, "strand": strand}
+    if swap_cigar:
+        align.cigar = align.cigar.replace("I", "?").replace("D", "I").replace("?", "D")
 
-    if return_seq:
-        ret["seq"] = seq
-
-    if return_cigar:
-        cigar = (align["cigar"] if not swap_cigar
-                 else align["cigar"].replace("I", "?").replace("D", "I").replace("?", "D"))
-
-        # NOTE: "cigar(revcomp(target[start:end])) == query" when strand == 1
-        ret["cigar"] = cigar
-
-        # NOTE: if you prefer "revcomp(cigar(target[start:end])) == query", then use below
-        #ret["cigar"] = cigar if strand == 0 else revcomp_cigar(cigar)
-
-    return ret
+    return align
 
 
 """
