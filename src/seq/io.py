090c4ad13d3bc9761b1279551b532f59fe418bff
yoshihikosuzuki <ys.neoteny@gmail.com>
2020-06-04 15:41:21 +0900

    small fix

--- src/seq/io.py
+++ src/seq/io.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, Tuple, List, Sequence
+from typing import Optional, Sequence, Tuple, List, Dict
 import numpy as np
 from logzero import logger
 from Bio.SeqIO.FastaIO import SimpleFastaParser
@@ -10,22 +10,17 @@ from .util import split_seq, ascii_to_phred
 
 
 @dataclass
-class SeqRecord(ExplicitRepr):
-    """Abstract class for a single sequence."""
+class FastaRecord(ExplicitRepr):
+    """Sequence with name."""
+    name: str
     seq: str
+
+    def __repr__(self):
+        return self._order_repr(["name", "seq"])
 
     @property
     def length(self) -> int:
         return len(self.seq)
-
-
-@dataclass
-class FastaRecord(SeqRecord):
-    """Sequence with name."""
-    name: str
-
-    def __repr__(self):
-        return self._order_repr(["name", "seq"])
 
 
 @dataclass
@@ -38,7 +33,8 @@ class FastqRecord(FastaRecord):
 
     @property
     def qual_phred(self) -> np.ndarray:
-        return np.array(list(map(ascii_to_phred, self.qual)), dtype=np.int8)
+        return np.array(list(map(ascii_to_phred, self.qual)),
+                        dtype=np.int8)
 
 
 @dataclass
@@ -61,17 +57,8 @@ class SeqInterval:
         return self.end - self.start
 
 
-@dataclass
-class DazzTrack(ExplicitRepr):
-    id: int
-    intervals: List[SeqInterval]
-    name: Optional[str] = None
-
-    def __repr__(self) -> str:
-        return self._order_repr(["id", "name", "intervals"])
-
-
-def load_fasta(in_fname: str, case: str = "original") -> List[FastaRecord]:
+def load_fasta(in_fname: str,
+               case: str = "original") -> List[FastaRecord]:
     """`case` must be one of {"original" (default), "lower", "upper"}."""
     assert case in ("original", "lower", "upper"), \
         "`case` must be 'original', 'lower', or 'upper'"
@@ -85,7 +72,8 @@ def load_fasta(in_fname: str, case: str = "original") -> List[FastaRecord]:
     return seqs
 
 
-def load_fastq(in_fname: str, case: str = "original") -> List[FastqRecord]:
+def load_fastq(in_fname: str,
+               case: str = "original") -> List[FastqRecord]:
     """`case` must be one of {"original" (default), "lower", "upper"}."""
     assert case in ("original", "lower", "upper"), \
         "`case` must be 'original', 'lower', or 'upper'"
@@ -100,7 +88,9 @@ def load_fastq(in_fname: str, case: str = "original") -> List[FastqRecord]:
     return seqs
 
 
-def save_fasta(reads: Sequence[FastaRecord], out_fname: str, width: int = -1):
+def save_fasta(reads: Sequence[FastaRecord],
+               out_fname: str,
+               width: int = -1):
     """If `width` > 0, newlines are inserted at every `width` bp."""
     assert width != 0, "`width` must not be 0"
     with open(out_fname, 'w') as f:
@@ -110,7 +100,8 @@ def save_fasta(reads: Sequence[FastaRecord], out_fname: str, width: int = -1):
             f.write(f">{read.name}\n{seq}\n")
 
 
-def save_fastq(reads: Sequence[FastqRecord], out_fname: str):
+def save_fastq(reads: Sequence[FastqRecord],
+               out_fname: str):
     with open(out_fname, 'w') as f:
         for read in reads:
             f.write(f"@{read.name}\n{read.seq}\n+\n{read.qual}\n")
@@ -121,7 +112,9 @@ def load_db(db_fname: str,
     """Load read IDs, original header names, and sequences from a DAZZ_DB file.
     `dbid_range` is e.g. `(1, 10)`, which is equal to `$ DBdump {db_fname} 1-10`.
     """
-    seqs = []
+    n_reads = db_to_n_reads(db_fname)
+    seqs = [None] * n_reads
+    i = 0
     command = (f"DBdump -rhs {db_fname} "
                f"{'' if dbid_range is None else '-'.join(map(str, dbid_range))}")
     for line in run_command(command).strip().split('\n'):
@@ -135,43 +128,34 @@ def load_db(db_fname: str,
             name = f"{prolog}/{well}/{start}_{end}"
         elif line.startswith('S'):
             _, _, seq = line.split()
-            seqs.append(DazzRecord(id=int(dazz_id),
-                                   name=name,
-                                   seq=seq))
-    logger.info(f"{len(seqs)} sequences loaded")
+            seqs[i] = DazzRecord(id=int(dazz_id), name=name, seq=seq)
+            i += 1
+    assert i == n_reads
+    logger.info(f"{n_reads} sequences loaded")
     return seqs
 
 
-def load_db_track(db_fname: str, track_name: str,
-                  dbid_range: Optional[Tuple[int, int]] = None,
-                  load_headers: bool = False) -> List[DazzTrack]:
+def load_db_track(db_fname: str,
+                  track_name: str,
+                  dbid_range: Optional[Tuple[int, int]] = None) \
+        -> Dict[int, List[SeqInterval]]:
     """Load track data of a DAZZ_DB file.
     Running a command like: `$ DBdump -r -m{track_name} {db_fname} {dbid_range}`.
-    If `load_headers` is True, sequence header names are loaded as well.
     """
-    tracks = []
-    command = (f"DBdump -r{'h' if load_headers else ''} -m{track_name} {db_fname} "
-               f"{'' if dbid_range is None else '-'.join(map(str, dbid_range))}")
-    name = None
+    tracks = {}
     count = 0
+    command = (f"DBdump -r -m{track_name} {db_fname} "
+               f"{'' if dbid_range is None else '-'.join(map(str, dbid_range))}")
     for line in run_command(command).strip().split('\n'):
         line = line.strip()
         if line.startswith('R'):
-            _, dazz_id = line.split()
-        elif line.startswith('H'):
-            _, _, prolog = line.split()
-        elif line.startswith('L'):
-            _, well, start, end = line.split()
-            name = f"{prolog}/{well}/{start}_{end}"
+            _, read_id = line.split()
         elif line.startswith('T0'):
-            poss = line.split()[2:]
+            poss = list(map(int, line.split()[2:]))
             assert len(poss) % 2 == 0, f"Cannot find pair of positions: {line}"
-            intervals = [SeqInterval(int(start), int(end))
-                         for start, end in zip(poss[::2], poss[1::2])]
-            tracks.append(DazzTrack(id=int(dazz_id),
-                                    intervals=intervals,
-                                    name=name))
-            count += len(intervals)
+            tracks[read_id] = [SeqInterval(start, end)
+                               for start, end in zip(poss[::2], poss[1::2])]
+            count += len(poss) // 2
     logger.info(f"{count} intervals loaded from {len(tracks)} sequences.")
     return tracks
 
@@ -187,4 +171,4 @@ def db_to_n_blocks(db_fname: str) -> int:
 
 def db_to_n_reads(db_fname: str) -> int:
     """Return the number of reads in a DAZZ_DB file."""
-    return int(run_command(f"DBdump {db_fname} | awk 'NR == 1 {{print $3}}'").strip())
+    return int(run_command(f"DBdump {db_fname}").split('\n')[0].split()[-1])
