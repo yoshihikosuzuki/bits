20a6e80715b53eda48af65c6da5dd15024de16b6
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-03-09 14:56:42 +0900

    update

--- src/seq/_dotplot.py
+++ src/seq/_dotplot.py
@@ -29,6 +29,7 @@ class DotPlot:
     optional arguments:
       @ tmp_dir : Directory for generating fasta files and plots.
     """
+
     gepard_root: InitVar[Optional[str]] = None
     gepard_jar: InitVar[Optional[str]] = None
     gepard_mat: InitVar[Optional[str]] = None
@@ -36,25 +37,39 @@ class DotPlot:
     tmp_dir: str = "tmp"
 
     def __post_init__(self, gepard_root, gepard_jar, gepard_mat):
-        assert gepard_root is not None or (gepard_jar is not None and gepard_mat is not None), \
-            "`gepard_root` or `gepard_[jar|mat]` must be specified"
-        _gepard_jar = (gepard_jar if gepard_jar is not None
-                       else f"{gepard_root}/dist/Gepard-1.40.jar")
-        _gepard_mat = (gepard_mat if gepard_mat is not None
-                       else f"{gepard_root}/resources/matrices/edna.mat")
-        self.gepard = ' '.join([f"java -cp {_gepard_jar}",
-                                "org.gepard.client.cmdline.CommandLine",
-                                f"-matrix {_gepard_mat}"])
+        assert gepard_root is not None or (
+            gepard_jar is not None and gepard_mat is not None
+        ), "`gepard_root` or `gepard_[jar|mat]` must be specified"
+        _gepard_jar = (
+            gepard_jar
+            if gepard_jar is not None
+            else f"{gepard_root}/dist/Gepard-1.40.jar"
+        )
+        _gepard_mat = (
+            gepard_mat
+            if gepard_mat is not None
+            else f"{gepard_root}/resources/matrices/edna.mat"
+        )
+        self.gepard = " ".join(
+            [
+                f"java -cp {_gepard_jar}",
+                "org.gepard.client.cmdline.CommandLine",
+                f"-matrix {_gepard_mat}",
+            ]
+        )
         run_command(f"mkdir -p {self.tmp_dir}")
 
-    def plot(self,
-             a_seqs: Optional[Union[str, Type[FastaRecord], List[Type[FastaRecord]]]],
-             b_seqs: Optional[Union[str, Type[FastaRecord], List[Type[FastaRecord]]]],
-             out_fname: Optional[str] = None,
-             word_size: int = 10,
-             fig_size: Union[int, Tuple[Optional[int], Optional[int]]] = 1000,
-             plot_size: int = 500,
-             interactive_plot: bool = False):
+    def plot(
+        self,
+        a_seqs: Optional[Union[str, Type[FastaRecord], List[Type[FastaRecord]]]],
+        b_seqs: Optional[Union[str, Type[FastaRecord], List[Type[FastaRecord]]]],
+        out_fname: Optional[str] = None,
+        word_size: int = 10,
+        only_plot: bool = False,
+        fig_size: Union[int, Tuple[Optional[int], Optional[int]]] = 1000,
+        plot_size: int = 500,
+        interactive_plot: bool = False,
+    ):
         """Draw a dot plot between two sequences.
 
         positional arguments:
@@ -63,37 +78,53 @@ class DotPlot:
         optional arguments:
           @ out_fname  : Output file name of the dot plot.
           @ word_size  : Word size for Gepard.
+          @ only_plot  : Show only the plot and not texts and margins.
           @ fig_size   : Size of the png file of the dot plot (in pixel).
           @ plot_size  : Display size of the plot image (in pixel).
         """
+
         def _prep(seqs: str, prolog: str):
-            if isinstance(seqs, str) and splitext(seqs)[1] == ".fasta":   # fasta file name
+            if (
+                isinstance(seqs, str) and splitext(seqs)[1] == ".fasta"
+            ):  # fasta file name
                 return seqs
 
             out_fasta = join(self.tmp_dir, f"{prolog}.fasta")
             if not isinstance(seqs, list):
                 seqs = [seqs]
-            save_fasta([seq if isinstance(seq, FastaRecord)
-                        else FastaRecord(name=f"{prolog}/{i}/0_{len(seq)}",
-                                         seq=seq)
-                        for i, seq in enumerate(seqs)],
-                       out_fasta)
+            save_fasta(
+                [
+                    (
+                        seq
+                        if isinstance(seq, FastaRecord)
+                        else FastaRecord(name=f"{prolog}/{i}/0_{len(seq)}", seq=seq)
+                    )
+                    for i, seq in enumerate(seqs)
+                ],
+                out_fasta,
+            )
             return out_fasta
 
-        a_fasta = _prep(a_seqs, 'a')
-        b_fasta = _prep(b_seqs, 'b')
+        a_fasta = _prep(a_seqs, "a")
+        b_fasta = _prep(b_seqs, "b")
         if out_fname is None:
             out_fname = f"{self.tmp_dir}/dotplot.png"
 
-        run_command(' '.join(["unset DISPLAY;",
-                              f"{self.gepard}",
-                              f"-seq1 {a_fasta}",
-                              f"-seq2 {b_fasta}",
-                              f"-maxwidth {fig_size}",
-                              f"-maxheight {fig_size}",
-                              f"-word {word_size}",
-                              f"-outfile {out_fname}"]))
-        show_image(out_fname,
-                   interactive=interactive_plot,
-                   width=plot_size,
-                   height=plot_size)
+        run_command(
+            " ".join(
+                [
+                    "unset DISPLAY;",
+                    f"{self.gepard}",
+                    f"-seq1 {a_fasta}",
+                    f"-seq2 {b_fasta}",
+                    f"-maxwidth {fig_size}",
+                    f"-maxheight {fig_size}",
+                    f"-word {word_size}",
+                    "-onlyplot" if only_plot else "",
+                    f"-outfile {out_fname}",
+                ]
+            )
+        )
+        show_image(
+            out_fname, interactive=interactive_plot, width=plot_size, height=plot_size
+        )
